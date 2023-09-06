817df24266bc2d888f974a4824461b045160a9c7
Khilchenko Yurii <38539304+KhilchenkoYurii@users.noreply.github.com>
2023-09-07 02:24:21 +0300

    [Backend][#52] Debugging
    By some reason does not work from Heroku

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -47,6 +47,7 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'PRODUCTION') {
