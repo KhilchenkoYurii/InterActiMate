d41e1dfbefee1978a921dbdd98a018e79efaa83b
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-09-27 23:23:44 +0300

    [Web]:[#33]: add chat previews

--- client/src/pages/chat/ChatPage.tsx
+++ client/src/pages/chat/ChatPage.tsx
@@ -1,19 +1,26 @@
 import { TitleWithIcons } from "../../components/PageTitleWithIcons/TitleWithIcons";
 import ChatIcon from "../../assets/icons/chat.svg";
+import { ReactComponent as TriangleHamburger } from "../../assets/icons/triangle-hamburger.svg";
 import './ChatPage.scss';
+import { ChatPreview } from "../../components/ChatPreview/ChatPreview";
 
 export const ChatPage = () => {
   return (
-    <>
-      <TitleWithIcons icon={ChatIcon} title="Чат" />
+    <div className="chat-page">
       <div className="chat-container">
-        <div className="users-container">
-          huj
+        <div className="users-container w-1/4 overflow-scroll">
+          <div className="users-container-header">
+            <TriangleHamburger />
+          </div>
+          {Array.apply(null, Array(15)).map(() => (
+            <ChatPreview name='KuYar' lastMessage='Without further interruption let`s celebrate and suck some dicks' />
+          ))}
         </div>
-        <div className="messages-container">
-          pizda
+        <div className="messages-container w-3/4">
+          {/* TODO: messages part */}
+          Messages
         </div>
       </div>
-    </>
+    </div>
   );
 };
