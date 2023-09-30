f357b53f1faf3dcba6b0eec5bd0caa6bb3aa7d92
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-10-01 02:23:45 +0300

    [Web]:[#33]: add sending and storing message

--- client/src/pages/chat/ChatPage.tsx
+++ client/src/pages/chat/ChatPage.tsx
@@ -4,27 +4,32 @@ import './ChatPage.scss';
 import { ChatPreview } from "../../components/ChatPreview/ChatPreview";
 import { ChatMessage } from "../../components/ChatMessage/ChatMessage";
 import { ChatInput } from "../../components/ChatInput/ChatInput";
-
-const MOCK_MESSAGES = [
-  { body: "I`ll be right happy to!" },
-  { body: "Can you show me?" },
-  { body: "Well, my daddy told me a few things to like uh, how not to rip the skin by using someone else`s mouth instead of your own hands." },
-  { body: "Sometimes I pull it on so hard I rip the skin" },
-  { body: "MMmMmmMmM" },
-  { body: "Yeah, I see that. Daddy gave you good advice!" },
-  { body: "It gets bigger when I pull on it" },
-  { body: "Sorry for what? Our daddy told us not to be ashamed of our dicks, especially since they are such good size and all" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-  { body: "Oh shit, I`m sorry" },
-];
+import { useEffect } from "react";
+import ApiService from "../../services/api.service";
+import { useDispatch, useSelector } from 'react-redux';
+import { chatSelector } from "../../store/chat/chat.selector";
+import { sendMessage } from "../../store/chat/chat.action";
 
 export const ChatPage = () => {
+  const dispatch = useDispatch();
+  const { currentChat } = useSelector(chatSelector);
+
+
+  const loadChats = async() => {
+    const usedId = localStorage.getItem('userId');
+    const chats = await ApiService.get(`chats/${usedId}`);
+    // TODO: remove log after displaying chats from API is implemented
+    console.log('chats fetched::', chats);
+  }
+
+  useEffect(() => {
+    loadChats();
+  }, []);
+
+  const handleSend = (message: string) => {
+    dispatch(sendMessage(message));
+  };
+
   return (
     <div className="chat-page">
       <div className="chat-container">
@@ -38,10 +43,10 @@ export const ChatPage = () => {
         </div>
         <div className="messages-container w-3/4 flex flex-col-reverse items-end pt-4 pr-2 pb-14 overflow-scroll">
           <div className="fixed bottom-0 right-0 message-input w-3/4 bg-white">
-            <ChatInput icon={SendIcon} />
+            <ChatInput icon={SendIcon} onIconClick={handleSend} />
           </div>
-          {MOCK_MESSAGES.map(({ body }) => 
-            <ChatMessage body={body} />
+          {currentChat.messages.map((message: string) => 
+            <ChatMessage body={message} />
           )}
         </div>
       </div>
