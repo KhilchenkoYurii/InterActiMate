ecbd18497204a6669bb56e1376e5a6b497baf190
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-10-02 04:01:04 +0300

    [Web]: Added styles for Card, and additional logic

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,18 +1,18 @@
-import './Card.scss';
-import { useEffect, useState } from 'react';
-import { ReactComponent as CalendarIcon } from '../../assets/icons/Calendar_light.svg';
-import { ReactComponent as DoneIcon } from '../../assets/icons/Done_all_alt_round_fill_line.svg';
-import { ReactComponent as UserIcon } from '../../assets/icons/User_alt_fill.svg';
-import { ReactComponent as EmailIcon } from '../../assets/icons/Email_Icon.svg';
-import { ReactComponent as PhoneIcon } from '../../assets/icons/PhoneIcon.svg';
-import { ReactComponent as NickNameIcon } from '../../assets/icons/NickName.svg';
-import { ButtonWithIcon } from '../ButtonWithIcon/ButtonWithIcon';
-import ApplyIcon from '../../assets/icons/Apply.svg';
-import ApiService from '../../services/api.service';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-import { notify } from '../../services/notification.service';
-import Placeholder from '../Placeholders/Placeholder';
+import "./Card.scss";
+import { useEffect, useState } from "react";
+import { ReactComponent as CalendarIcon } from "../../assets/icons/Calendar_light.svg";
+import { ReactComponent as DoneIcon } from "../../assets/icons/Done_all_alt_round_fill_line.svg";
+import { ReactComponent as UserIcon } from "../../assets/icons/User_alt_fill.svg";
+import { ReactComponent as EmailIcon } from "../../assets/icons/Email_Icon.svg";
+import { ReactComponent as PhoneIcon } from "../../assets/icons/PhoneIcon.svg";
+import { ReactComponent as NickNameIcon } from "../../assets/icons/NickName.svg";
+import { ButtonWithIcon } from "../ButtonWithIcon/ButtonWithIcon";
+import ApplyIcon from "../../assets/icons/Apply.svg";
+import ApiService from "../../services/api.service";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+import { notify } from "../../services/notification.service";
+import Placeholder from "../Placeholders/Placeholder";
 
 //TODO: Create an Interface for the props
 export const Card = ({
@@ -30,11 +30,11 @@ export const Card = ({
   postId,
   attachments,
 }: any) => {
-  const userId = 'USR3';
+  const userId = localStorage.getItem("userId");
   const [isApplied, setApplying] = useState<Boolean | undefined>(undefined);
 
   const getDate = (date: string): string | undefined => {
-    return new Date(date).toLocaleString('ru-RU').split(',').shift();
+    return new Date(date).toLocaleString("ru-RU").split(",").shift();
   };
 
   useEffect(() => {
@@ -46,7 +46,7 @@ export const Card = ({
   const applyPost = () => {
     ApiService.post(`posts/answerPost`, { userId, postId });
     setApplying(true);
-    notify({ duration: 1200, title: 'Заявку подано!' });
+    notify({ duration: 1200, title: "Заявку подано!" });
   };
 
   return (
@@ -133,34 +133,32 @@ export const Card = ({
               <div
                 className={
                   isApplied
-                    ? 'card-apply-btn-not-allowed prevent-select'
-                    : 'card-apply-btn'
+                    ? "card-apply-btn-not-allowed prevent-select"
+                    : "card-apply-btn"
                 }
               >
                 <ButtonWithIcon
                   buttonType="outline"
-                  text={isApplied ? 'Заявку подано' : 'Подати заявку'}
+                  text={isApplied ? "Заявку подано" : "Подати заявку"}
                   icon={isApplied ? null : ApplyIcon}
                   onClick={() => applyPost()}
                 />
               </div>
             </div>
-            <div className="card-container card-imgs">
-              <AwesomeSlider
-                bullets={false}
-                infinite={attachments.length > 0 ? true : false}
-                organicArrows={attachments.length > 0 ? true : false}
-                className="aws-btn"
-              >
-                {attachments.length > 0 ? (
-                  attachments.map((e: any) => (
+            {attachments.length > 0 && (
+              <div className="card-container card-imgs">
+                <AwesomeSlider
+                  bullets={false}
+                  infinite={attachments.length > 0 ? true : false}
+                  organicArrows={attachments.length > 0 ? true : false}
+                  className="aws-btn"
+                >
+                  {attachments.map((e: any) => (
                     <div key={e.alt} data-src={e.address} />
-                  ))
-                ) : (
-                  <div data-src="https://thegravix.com/wp-content/uploads/2022/06/fuck_you-ctena.jpg" />
-                )}
-              </AwesomeSlider>
-            </div>
+                  ))}
+                </AwesomeSlider>
+              </div>
+            )}
           </div>
         </>
       ) : (
