a7f77105fd8330c180de954fe845e110ec941aec
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-10-04 23:42:52 +0300

    [Web]: Added fix, and updated  MyReqPage

--- client/src/pages/my-requests/MyRequestsPage.tsx
+++ client/src/pages/my-requests/MyRequestsPage.tsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import MyRequests from '../../components/MyRequests/MyRequests';
 import constants from '../../services/constants';
 import { Tabs } from '../../components/Tabs/Tabs';
+import { TitleWithIcons } from '../../components/PageTitleWithIcons/TitleWithIcons';
 
 function MyRequestsPage() {
   const navigate = useNavigate();
@@ -34,6 +35,31 @@ function MyRequestsPage() {
     })();
   }, []);
 
+  const getNoRequestsView = (canceledReq?: any) => {
+    return (
+      <div className="flex flex-col justify-center items-center mt-5">
+        <div className="font-semibold">Активних оголошень не знайдено</div>
+        <span>
+          Щоб створити нове оголошення натисніть{' '}
+          <Link className="underline" to="/add-request">
+            сюди
+          </Link>
+          {canceledReq?.length > 0 && (
+            <span>
+              , або відвідайте{' '}
+              <span
+                className="underline cursor-pointer"
+                onClick={() => setTabName(tabs[1].name)}
+              >
+                архів
+              </span>
+            </span>
+          )}
+        </span>
+      </div>
+    );
+  };
+
   const getMyRequestPageView = () => {
     if (requests.length > 0) {
       let activeReq = requests.filter(
@@ -49,30 +75,15 @@ function MyRequestsPage() {
       if (canceledReq.length > 0 && tabName === tabs[1].name) {
         return <MyRequests requests={canceledReq} />;
       }
-      if (!activeReq.length)
-        return (
-          <div className="flex flex-col justify-center items-center mt-5">
-            <div className="font-semibold">Активних оголошень не знайдено</div>
-            <span>
-              Щоб створити нове оголошення натисніть{' '}
-              <Link className="underline" to="/add-request">
-                сюди
-              </Link>
-              {canceledReq.length > 0 && (
-                <span>
-                  , або відвідайте{' '}
-                  <span
-                    className="underline cursor-pointer"
-                    onClick={() => setTabName(tabs[1].name)}
-                  >
-                    архів
-                  </span>
-                </span>
-              )}
-            </span>
-          </div>
-        );
+      if (!activeReq.length) return getNoRequestsView(canceledReq);
     }
+    if (!requests.length)
+      return (
+        <div>
+          <TitleWithIcons title="Мої оголошення" />
+          {getNoRequestsView()}
+        </div>
+      );
   };
 
   return (
