e2b5d3156c5ab4cb8def93fa3f961a427c8922fd
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-10-17 03:05:17 +0300

    [Web]: Costul

--- client/src/pages/auth/setCookieHandler.tsx
+++ client/src/pages/auth/setCookieHandler.tsx
@@ -16,14 +16,18 @@ function setCookieHandler(token: string): void {
 }
 
 function clearCookieHandler() {
-  console.log('Domain', DOMAIN);
+  console.log('DomainNew', DOMAIN);
   const cookies = document.cookie.split(';');
 
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i];
     const eqPos = cookie.indexOf('=');
     const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    document.cookie =
+      name + `=; domain=${DOMAIN || ''}; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    document.cookie =
+      name +
+      `=; domain=.${DOMAIN || ''}; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
   }
 }
 
