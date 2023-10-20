3a2687f414b8304c2f0febbefebf705c0efb7408
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-10-20 03:01:42 +0300

    [Web]: Added responsive MyRequest page

--- client/src/components/MyRequests/MyRequests.tsx
+++ client/src/components/MyRequests/MyRequests.tsx
@@ -21,78 +21,80 @@ function MyRequests({ requests }: IMyRequests) {
   };
 
   return (
-    <div className="flex flex-wrap justify-center items-center flex-col my-3">
+    <div className="flex flex-col justify-center items-center my-3">
       {requests.map((req: IRequestCard) => (
         <div
           key={req._id}
-          className="reqs-container gap-3 my-3"
+          className="grid grid-rows-2 reqs-container gap-3 my-3"
           onClick={() => navigateToRequest(req._id)}
         >
-          <div>
+          <div className="w-full md:max-w-[40rem]">
             <div className="text-title">{req.title}</div>
-            <div className="break-words w-[25rem] text-[0.9rem]">
-              {req.body}
-            </div>
+            <div className="break-words  text-[0.9rem]">{req.body}</div>
             <span className="reqs-date">{getDate(req.dateOfCreation)}</span>
             <hr className="max-w-[8rem] mt-2 mb-3" />
             <div className="flex gap-2 flex-wrap">
-              <Button disabled variant="contained">
+              {/* <Button disabled variant="contained">
                 Редагувати
-              </Button>
+              </Button> */}
               {req.status === constants.postStatus.active ? (
-                <Button
-                  variant="contained"
-                  onClick={async (e) => {
-                    e.stopPropagation();
-                    let answer = window.confirm(
-                      `Ви впевнені що хочете деактивувати "${req.title}"?`,
-                    );
+                <div className="w-full md:flex">
+                  <button
+                    onClick={async (e) => {
+                      e.stopPropagation();
+                      let answer = window.confirm(
+                        `Ви впевнені що хочете деактивувати "${req.title}"?`,
+                      );
 
-                    if (answer) {
-                      try {
-                        await apiService.put(`posts/${req.postId}`, {
-                          status: constants.postStatus.canceled,
-                        });
-                        window.location.reload();
-                      } catch (error) {
-                        console.log('Error: ', error);
+                      if (answer) {
+                        try {
+                          await apiService.put(`posts/${req.postId}`, {
+                            status: constants.postStatus.canceled,
+                          });
+                          window.location.reload();
+                        } catch (error) {
+                          console.log('Error: ', error);
+                        }
                       }
-                    }
-                  }}
-                  color="error"
-                >
-                  Деактивувати
-                </Button>
+                    }}
+                    type="button"
+                    className="text-[white] bg-[#d32f2f] rounded-[4px] flex justify-center items-center h-10 min-w-[10rem]"
+                  >
+                    Деактивувати
+                  </button>
+                </div>
               ) : (
-                <Button
-                  variant="contained"
-                  onClick={async (e) => {
-                    e.stopPropagation();
-                    let answer = window.confirm(
-                      `Ви впевнені що хочете активувати "${req.title}"?`,
-                    );
+                <div className="w-full md:flex">
+                  <button
+                    type="button"
+                    className="text-[white] bg-[green] rounded-[4px] flex justify-center items-center h-10 min-w-[10rem]"
+                    onClick={async (e) => {
+                      e.stopPropagation();
+                      let answer = window.confirm(
+                        `Ви впевнені що хочете активувати "${req.title}"?`,
+                      );
 
-                    if (answer) {
-                      try {
-                        await apiService.put(`posts/${req.postId}`, {
-                          status: constants.postStatus.active,
-                        });
-                        window.location.reload();
-                      } catch (error) {
-                        console.log('Error: ', error);
+                      if (answer) {
+                        try {
+                          await apiService.put(`posts/${req.postId}`, {
+                            status: constants.postStatus.active,
+                          });
+                          window.location.reload();
+                        } catch (error) {
+                          console.log('Error: ', error);
+                        }
                       }
-                    }
-                  }}
-                  color="success"
-                >
-                  Активувати
-                </Button>
+                    }}
+                  >
+                    Активувати
+                  </button>
+                </div>
               )}
             </div>
           </div>
-          <div>
+          <div className="w-full md:w-auto">
             <img
-              className="w-[10rem] h-[10rem] object-cover rounded-md"
+              className="m-auto w-[20rem] h-[20rem] md:w-[10rem] md:h-[10rem] md:m-0 object-cover rounded-md"
               src={
                 req.attachments[0]
                   ? req.attachments[0].address
