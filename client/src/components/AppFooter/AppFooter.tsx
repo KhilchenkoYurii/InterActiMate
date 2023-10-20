5ce8e96890b3225474ada08b2b852cb6873c5d07
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-10-21 00:16:34 +0300

    [WEB]: fix footer banner overlap

--- client/src/components/AppFooter/AppFooter.tsx
+++ client/src/components/AppFooter/AppFooter.tsx
@@ -3,7 +3,7 @@ import "./AppFooter.scss";
 
 
 export const AppFooter = () => (
-  <div className="app-footer flex justify-center items-center fixed left-0 bottom-0">
+  <div className="app-footer flex justify-center items-center fixed left-0 bottom-0 z-50">
     <div className="text-xl	text-yellow-500 leading-4	font-black mx-2 hidden md:flex width-210">ВЕБСАЙТ ПРАЦЮЄ В ТЕСТОВОМУ РЕЖИМІ</div>
       <img src={Images.IAM} className="w-10 h-10" />
       <img src={Images.UpSHift} className="w-10 h-10" />
