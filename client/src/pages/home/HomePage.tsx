33f662e3a32ca3f8fc1339e10d49733f33182447
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-10-22 23:25:30 +0300

    [WEB]: upd RequestCards grid max columns

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -92,7 +92,7 @@ export const HomePage = () => {
   };
 
   return (
-    <div>
+    <div className='flex justify-center home-page-container'>
       {getTabsView()}
       {/* there's no sense to check possible numbers of columns for mobile, so it's hardcoded */}
       {isMobile && <div className={`cards-container grid grid-cols-2`}>{getRequestsView()}</div>}
