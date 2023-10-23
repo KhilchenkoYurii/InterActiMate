696f0a601bbb4be44ab37eaece3919e30b4c6f6e
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-10-23 10:37:49 +0300

    [WEB]: add opening request from chat in new tab

--- client/src/components/RequestLinkCard/RequestLinkCard.tsx
+++ client/src/components/RequestLinkCard/RequestLinkCard.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import constants from "../../services/constants";
 import NewTabIcon from '../../assets/icons/NewTab.svg';
 
@@ -8,23 +8,16 @@ interface IRequestLink {
   postId: string;
 }
 
-export const RequestLinkCard = ({ img, title, postId }: IRequestLink) => {
-  const navigate = useNavigate();
-  const handlePress = () => {
-    navigate(`/request?id=${postId}`);
-  };
-
-  return (
-    <div className="flex items-center h-14 w-full bg-white cursor-pointer shadow-sm" onClick={handlePress}>
-      <div className="h-14 w-14 overflow-hidden">
-        <img src={img || constants.imageNotFound} className="h-14 scale-125	" />
-      </div>
-      <div className="flex pr-2 w-full h-full justify-between pr-2">
-        <div className="flex items-center text-left">{title}</div>
-        <div className="flex h-full">
-          <img src={NewTabIcon} className="w-4 h-4 mt-1" />
-        </div>
+export const RequestLinkCard = ({ img, title, postId }: IRequestLink) => (
+  <Link to={`/request?id=${postId}`} target="_blank" rel="noopener noreferrer" className="flex items-center h-14 w-full bg-white cursor-pointer shadow-sm">
+    <div className="h-14 w-14 overflow-hidden">
+      <img src={img || constants.imageNotFound} className="h-14 scale-125	" />
+    </div>
+    <div className="flex pr-2 w-full h-full justify-between pr-2">
+      <div className="flex items-center text-left">{title}</div>
+      <div className="flex h-full">
+        <img src={NewTabIcon} className="w-4 h-4 mt-1" />
       </div>
     </div>
-  );
-};
+  </Link>
+);
