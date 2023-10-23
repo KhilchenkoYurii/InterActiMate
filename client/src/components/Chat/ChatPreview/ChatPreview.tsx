3dc2ac547b64e0ef75231685551733aeb2e2ffcf
AndrewIatskiv <andriy.iatskiv@serious-software.com>
2023-10-23 17:32:32 +0300

    [WEB]: upd chat default user image

--- client/src/components/Chat/ChatPreview/ChatPreview.tsx
+++ client/src/components/Chat/ChatPreview/ChatPreview.tsx
@@ -1,3 +1,5 @@
+import NoImageUser from '../../../assets/icons/user_filled.svg';
+
 interface IChatPreview {
   isActive: boolean;
   img?: string;
@@ -6,9 +8,7 @@ interface IChatPreview {
   onChatClick: () => void;
 }
 
-const defaultImageUrl = 'https://online.hitpaw.com/images/topics/face-animator/who-is-gigachad.jpg';
-
-export const ChatPreview = ({ isActive, img = defaultImageUrl, name, lastMessage, onChatClick }: IChatPreview) => {
+export const ChatPreview = ({ isActive, img = NoImageUser, name, lastMessage, onChatClick }: IChatPreview) => {
   return (
     <div className={`flex chat-preview w-100 h-20 p-2 flex-1 sm:flex-col md:flex-row align-center gap-1 flex-nowrap cursor-pointer ${isActive && 'bg-cyan-700 text-white'}`} onClick={onChatClick}>
       <img src={img} className='h-16 w-16 rounded-full' />
