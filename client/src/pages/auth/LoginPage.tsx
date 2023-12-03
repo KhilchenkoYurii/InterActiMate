73daa7794dc4eca1b24cef1be21aaa8393a65460
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-12-03 23:41:38 +0200

    Test case

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,25 +1,26 @@
-import { useNavigate } from "react-router-dom";
-import Login from "../../components/Auth/Login/Login";
-import apiService from "../../services/api.service";
-import { notify } from "../../services/notification.service";
-import { setCookieHandler } from "./setCookieHandler";
+import { useNavigate } from 'react-router-dom';
+import Login from '../../components/Auth/Login/Login';
+import apiService from '../../services/api.service';
+import { notify } from '../../services/notification.service';
+import { setCookieHandler } from './setCookieHandler';
 
 export const LoginPage = () => {
   const nav = useNavigate();
 
   const onSubmitLogin = async (email: string, password: string) => {
     try {
-      const { data } = await apiService.post("users/login", {
+      const data = await apiService.post('users/login', {
         email,
         password,
       });
-      setCookieHandler(data.token);
-      localStorage.setItem("userId", data.data.user.userId);
-      nav("/");
+      console.log('data :', data);
+      // setCookieHandler(data.token);
+      // localStorage.setItem('userId', data.data.user.userId);
+      nav('/');
     } catch (error: any) {
-      console.log("Error: ", error);
+      console.log('Error: ', error);
       notify({
-        type: "danger",
+        type: 'danger',
         title: error.response.data.message,
         duration: 1000,
       });
