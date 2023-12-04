357ac8f5536f546b94c17fe85128934c663faf37
Yaroslav <kuzivyaroslav1999@gmail.com>
2023-12-04 03:44:54 +0200

    [Web] Added updated categories

--- client/src/components/AddEditRequest/AddEditRequest.tsx
+++ client/src/components/AddEditRequest/AddEditRequest.tsx
@@ -31,16 +31,28 @@ const AddEditRequest = ({ onSubmit }: any) => {
 
   const [title, setTitle] = useState<string>('');
   const [body, setBody] = useState<string>('');
-  const [href, setHref] = useState<string>('');
+  // const [href, setHref] = useState<string>('');
   const [file, setFile] = useState<IFile>();
   const [attachments, setAttachments] = useState<IAttachments[]>([]);
-  const [currCategorie, setCurrCategorie] = useState<string>('');
   const [categories, setCategories] = useState<string[]>([]);
+  const [defCategories, setDefCategories] = useState<string[]>([]);
   const [error, setErrors] = useState({ title: true });
   const [searchParams] = useSearchParams();
   const [isEdit, setIsEdit] = useState(false);
   const [isImgLoading, setIsImgLoading] = useState(false);
   const nav = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: {
+          data: { categories },
+        },
+      } = await apiService.get(`categories`);
+
+      setDefCategories([...categories.map((e: any) => e.name)]);
+    })();
+  }, []);
 
   useEffect(() => {
     let postId = searchParams.get('postId');
@@ -115,8 +127,16 @@ const AddEditRequest = ({ onSubmit }: any) => {
 
   const removeItem = (index: number, isAttachments: boolean) => {
     let temp = [...(isAttachments ? attachments : categories)] as any;
+    if (!isAttachments) {
+      setDefCategories((prev) => [...prev, categories[index]]);
+    }
     temp.splice(index, 1);
     isAttachments ? setAttachments(temp) : setCategories(temp);
+  };
+
+  const addCategorie = (elem: string, index: number) => {
+    setCategories((prev: any): any => [...prev, elem]);
+    defCategories.splice(index, 1);
   };
 
   useEffect(() => {
@@ -198,50 +218,50 @@ const AddEditRequest = ({ onSubmit }: any) => {
         <div className="card-container relative mx-3">
           <div className="pt-10 pb-1">
             {getBlockTitleView('Категорії')}
-            <div className="flex flex-col gap-1 text-left">
-              <div className="flex flex-row gap-2">
-                <input
-                  type="text"
-                  value={currCategorie}
-                  onChange={(e) =>
-                    !/\s/g.test(currCategorie) &&
-                    setCurrCategorie(e.target.value.replace(/\s/g, ''))
-                  }
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter' && currCategorie !== '') {
-                      e.preventDefault();
-                      setCategories((prev: any): any => [
-                        ...prev,
-                        currCategorie,
-                      ]);
-                      setCurrCategorie('');
-                    }
-                  }}
-                  className="w-full bg-[#d9d9d938] rounded-[4px] border-[1px] border-[#00000033] p-[10px] outline-0 resize-none"
-                  placeholder="Категорії"
-                />
-
-                <img
-                  className="cursor-pointer"
-                  onClick={(e: any) => {
-                    if (currCategorie !== '') {
-                      e.preventDefault();
-                      setCategories((prev: any): any => [
-                        ...prev,
-                        currCategorie,
-                      ]);
-                      setCurrCategorie('');
-                    }
-                  }}
-                  src={AddIcon}
-                  alt="Add"
-                />
+            {/* DefCategories */}
+            {defCategories.length > 0 && (
+              <div className="text-[25px] mb-3">
+                Натисніть будь ласка лапкою щоб додати існуючу категорію
               </div>
-
-              {getNoteView({
-                text: '(Щоб додати категорію натисніть Enter, пробіл заборонений)',
-                className: 'text-[#176b87] font-extralight text-sm',
-              })}
+            )}
+            <div className="flex flex-col gap-1 text-left">
+              <div className="card-categories">
+                {defCategories.map((categorie: string, index: number) => (
+                  <span
+                    className="flex flex-row-reverse svg-hover select-none cursor-pointer"
+                    onClick={() => addCategorie(categorie, index)}
+                  >
+                    <div className="flex justify-center text-center items-center gap-1">
+                      {categorie}
+                      <span key={index}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12ZM12 18C11.4477 18 11 17.5523 11 17V13H7C6.44772 13 6 12.5523 6 12C6 11.4477 6.44772 11 7 11H11V7C11 6.44772 11.4477 6 12 6C12.5523 6 13 6.44772 13 7V11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H13V17C13 17.5523 12.5523 18 12 18Z"
+                            fill="#176B87"
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                  </span>
+                ))}
+              </div>
+            </div>
+            {/* Categories */}
+            {categories.length > 0 && (
+              <div>
+                {defCategories.length > 0 && <hr className="mt-5" />}
+                <div className="text-[25px]">Додані категорії</div>
+              </div>
+            )}
+            <div className="flex flex-col gap-1 text-left">
               <div className="card-categories">
                 {categories.map((categorie: string, index: number) => (
                   <span
